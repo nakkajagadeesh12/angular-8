e7711fe36965e47d6b1ee32618cdb1cc6243401e
nakkajagadeesh12 <nakkajagadeesh1234@gmail.com>
2020-05-25 14:24:47 -0700

    2 files modified

--- src/app/hello.component.ts
+++ src/app/hello.component.ts
@@ -1,14 +1,43 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input } from "@angular/core";
 
 @Component({
-  selector: 'hello',
-  template: `<h1>Hello {{name}}</h1>`,
-  styles: [`h1 { 
-    font-family: Lato;
-    color : violet;
-    font-size : 30px;
-     }`]
+  selector: "hello",
+  template: `
+    <h1>Hello {{ name }}</h1>
+    <h2>{{ fun() }}</h2>
+    <input [disabled]="false" type="text" />
+    <div [class.div1]="val">Hello</div>
+    <button (click)="fun1($event)">click</button>
+    <br />
+    <div *ngIf ="val then ifblock;else elseblock"></div>
+    <ng-template #ifblock>
+    <h1>visible</h1>
+    </ng-template>
+    <ng-template #elseblock>
+    <h1>hidden</h1>
+    </ng-template>
+  `,
+  styles: [
+    `
+      h1 {
+        font-family: Lato;
+        color: violet;
+        font-size: 30px;
+      }
+      .div1 {
+        color: blue;
+      }
+    `
+  ]
 })
-export class HelloComponent  {
+export class HelloComponent {
   @Input() name: string;
+  public str: string = "Jagath";
+  public val: boolean = false;
+  fun() {
+    return "hii " + this.str;
+  }
+  fun1(e){
+    console.log("button",e);
+  }
 }
