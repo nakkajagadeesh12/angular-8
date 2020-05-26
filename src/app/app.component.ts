02606c4372b76c6ec9e05f2a84ab4fddbed57a56
nakkajagadeesh12 <nakkajagadeesh1234@gmail.com>
2020-05-25 22:11:39 -0700

    2 files modified, 1 added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, VERSION } from "@angular/core";
+import { User } from "./user";
 
 @Component({
   selector: "my-app",
@@ -8,4 +9,12 @@ import { Component, VERSION } from "@angular/core";
 export class AppComponent {
   name: string = "Angular " + VERSION.full;
   technologies = ["Angular", "React", "SpringBoot", "Django"];
+  userModel = new User(
+    "Jagath",
+    "nakka@gmail.com",
+    3342222,
+    "",
+    "morning",
+    true
+  );
 }
---
+++ src/app/user.ts
@@ -0,0 +1,10 @@
+export class User {
+  constructor(
+    public name: string,
+    public email: string,
+    public phone: number,
+    public technology: string,
+    public timePreference: string,
+    public offers: boolean
+  ) {}
+}
