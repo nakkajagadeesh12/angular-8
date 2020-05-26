8d3485e24a4b4de64649df09e1b7852789885a7a
nakkajagadeesh12 <nakkajagadeesh1234@gmail.com>
2020-05-25 19:54:16 -0700

    3 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component, VERSION } from "@angular/core";
   styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  name : string = "Angular " + VERSION.full;
+  name: string = "Angular " + VERSION.full;
+  technologies = ["Angular", "React", "SpringBoot", "Django"];
 }
